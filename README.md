a53125bed8e094f6f6211e89ab6a9aecdc84025f
apporoad <apporoadina@163.com>
2020-05-09 17:56:47 +0800

    some

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # marry.js
 they match
+
+betrothal
+benches
+
+love
+law 
+must
+pluses
+
+
+institutionOfMarriage  1:1
---
+++ test/test.js
@@ -0,0 +1,15 @@
+
+
+var persons = [
+    { name : 'apporoad' , gender : 1},
+    { name : 'lilei' , gender : 1},
+    { name : 'jay' , gender : 1},
+    { name : 'bench' , gender : 1},
+    { name : 'jack' , gender : 1},
+    { name : 'luna' , gender : 2},
+    { name : 'LiSA' , gender : 2},
+    { name : 'aoer' , gender : 2},
+    { name : 'ishabella' , gender : 2},
+    { name : 'lute' , gender : 2},
+    { name : 'huimei' , gender : 2}
+]
